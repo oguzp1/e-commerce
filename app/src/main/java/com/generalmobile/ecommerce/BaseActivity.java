d094ec3003d23d89a880f755388f245f539310ec
Oguz Paksoy <oguz_paksoy_@hotmail.com>
2017-07-07 17:56:52 +0300

    #1 Navigation Drawer and Toolbar added

---
+++ app/src/main/java/com/generalmobile/ecommerce/BaseActivity.java
@@ -0,0 +1,52 @@
+package com.generalmobile.ecommerce;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+
+/**
+ * Created by MONSTER on 7.7.2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void createMenu(Toolbar toolbar, DrawerLayout drawerLayout) {
+        this.toolbar = toolbar;
+        this.drawerLayout = drawerLayout;
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+}
--- app/src/main/java/com/generalmobile/ecommerce/MainActivity.java
+++ app/src/main/java/com/generalmobile/ecommerce/MainActivity.java
@@ -1,15 +1,28 @@
 package com.generalmobile.ecommerce;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation)
+    NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "deneme", Toast.LENGTH_SHORT).show();
+        ButterKnife.bind(this);
+        createMenu(toolbar, drawerLayout);
     }
 }
