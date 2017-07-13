1429fc0106c80b2cc54fbaa8770c9afcafc62da3
btstajyer7 <btstajyer@gmail.com>
2017-07-13 14:23:44 +0300

    Screen15 created.

---
+++ app/src/main/java/com/generalmobile/ecommerce/screens/screen15/Screen15Activity.java
@@ -0,0 +1,15 @@
+package com.generalmobile.ecommerce.screens.screen15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.generalmobile.ecommerce.R;
+
+public class Screen15Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen15);
+    }
+}
---
+++ app/src/main/java/com/generalmobile/ecommerce/screens/screen15/Screen15Presenter.java
@@ -0,0 +1,8 @@
+package com.generalmobile.ecommerce.screens.screen15;
+
+/**
+ * Created by btstajyer7 on 13.07.2017.
+ */
+
+public class Screen15Presenter {
+}
